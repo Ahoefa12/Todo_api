3241134347feb448d30601649adffd21430c99df
Isabella BABATUNDE <bellabaly06@gmail.com>
2025-10-13 11:59:30 +0000

    Création et liste de projet

---
+++ app/Http/Controllers/Api/Project/ProjectController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api\Project;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Project::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $data = $request->validate ([
+                "name" => "required|string|min:4",
+                "description" => "required|string|min:4",
+                "status" => "required|string",
+            ]);
+            $project = Project::create($data );
+            return response()->json([
+                'message'=> 'Projet créé avec succes',
+                'data'=> $project
+            ],200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message'=> 'Erreur lors de la création' .$th->getMessage(),    
+            ],400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
